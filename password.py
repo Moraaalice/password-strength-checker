ae15570645d7fb8f8ae22730af21c49da80e90c9
moraaalice <alicemoraaongongo>
2023-06-14 14:01:20 +0300

    Added comments so that my code could be more readable

--- password.py
+++ password.py
@@ -1,11 +1,15 @@
-import string
+import string  
+#String is for accessing the String module for string constants
 import getpass
+#Get pass module for getting user input
 def check_password_strength():
    password = getpass.getpass('Enter the password: ')
+   #The line prompts the user to enter a password securely without displaying the input on the sscreen
    strength = 0
    remarks = ''
    lower_count = upper_count = num_count = wspace_count = special_count = 0
    for char in list(password):
+       #Iterating over each password and converting it to a list
        if char in string.ascii_lowercase:
            lower_count += 1
        elif char in string.ascii_uppercase:
@@ -16,6 +20,8 @@ def check_password_strength():
            wspace_count += 1
        else:
            special_count += 1
+           #This section checks each character in the password and increament the corresponding counter variables based
+           #on characters category ,uppercase,lowercase,digits
 
 
    if lower_count >= 1:
@@ -29,6 +35,8 @@ def check_password_strength():
    if special_count >= 1:
        strength += 1  
        
+       #These lines increment the strength variable based on the presence of different character types in the password.
+       # Each character type found in the password increments the strength by 1.
        
        
    if strength == 1: 
@@ -48,6 +56,9 @@ def check_password_strength():
 
 
 
+#These lines assign the appropriate remark based on the strength of the password, determined by the value of the 
+# strength variable.
+
    print('Your password has:-')
    print(f'{lower_count} lowercase letters')
    print(f'{upper_count} uppercase letters')
@@ -57,7 +68,6 @@ def check_password_strength():
    print(f'Password Score: {strength / 5}')
    print(f'Remarks: {remarks}') 
    
-
 def check_pwd(another_pw=False):
        valid = False
        if another_pw:
@@ -73,6 +83,25 @@ def check_pwd(another_pw=False):
                    print('Exiting...')
                return False
            else:
+               print('Invalid input...please try again. \n')
+
+
+#These lines define a function named check_pwd() which prompts the user to check the strength of another password. 
+# The another_pw parameter is used to determine if it's the first password check or a subsequent check. The function 
+# returns True if the user wants to check another password, and False otherwise.
+
+if __name__ == '__main__':
+   print('===== Welcome to Password Strength Checker =====')
+   check_pw = check_pwd()
+   while check_pw:
+       check_password_strength()
+       check_pw = check_pwd(True)
+       
+       
+#These lines are the main entry point of the program. It starts by printing a welcome message. 
+# Then, it calls the check_pwd() function to determine if the user wants to check their password strength.
+# If the user wants to check, it calls the check_password_strength() function. The loop continues until the user
+# no longer wants to check passwords.       
                
 
    
